fcdaefcc786d4a67b0e5a0c5f250522b88ad90d3
Clement.R <rondeau.clement@gmail.com>
2024-11-15 22:24:53 +0100

    + create scenes and import TMP pro

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int seed = 0;
+        Random.InitState(seed);
+
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.linearVelocity = Vector3.right;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
