49b0a5f41dc106224e586aef2e41090203b3fdf7
Clement.R <rondeau.clement@gmail.com>
2024-11-16 16:07:57 +0100

    * add collider to Potato prefab and implement shooting and crosshair mask wip

--- Assets/ShooterGameplayManager.cs
+++ Assets/ShooterGameplayManager.cs
@@ -4,10 +4,16 @@ using UnityEngine.UI;
 using System;
 using DG.Tweening;
 
+using UnityEngine.InputSystem;
+
 using Random = UnityEngine.Random;
 
 public class ShooterGameplayManager : MonoBehaviour
 {
+    [Header("Game")]
+    private float lel;
+
+    [Header("UI")]
     [SerializeField]
     private float m_fadeDuration = 1f;
 
@@ -17,6 +23,9 @@ public class ShooterGameplayManager : MonoBehaviour
     private Button m_startButton;
     [SerializeField]
     private CanvasGroup m_menuCanvasGroup;
+
+    private bool m_huntStarted = false;
+    private float m_huntStartTime = float.NaN;
 
     void Awake()
     {
@@ -28,6 +37,33 @@ public class ShooterGameplayManager : MonoBehaviour
         ShowMenu();
     }
 
+    private void Update()
+    {
+        // Timer before start
+        if (!m_huntStarted)
+        {
+            return;
+        }
+
+        if (m_huntStartTime + 3 > Time.time)
+        {
+            return;
+        }
+
+        // Try to shoot
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Ray rayOrigin = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(rayOrigin, out hitInfo))
+            {
+                GameObject hitGo = hitInfo.collider.gameObject;
+                Debug.Log(hitGo.name);
+            }
+        }
+    }
+
     private void StartHunt()
     {
         if (string.IsNullOrEmpty(m_nameInput.text))
@@ -37,6 +73,11 @@ public class ShooterGameplayManager : MonoBehaviour
 
         var hash = m_nameInput.text.GetPotatoHash();
         Random.InitState(hash);
+
+        HideMenu();
+
+        m_huntStarted = true;
+        m_huntStartTime = Time.time;
     }
 
     private void ShowMenu()
@@ -53,7 +94,7 @@ public class ShooterGameplayManager : MonoBehaviour
     private void HideMenu()
     {
         m_menuCanvasGroup
-        .DOFade(1f, m_fadeDuration)
+        .DOFade(0f, m_fadeDuration)
         .OnComplete(
             () => { SetMenuUsable(false); }
         );
