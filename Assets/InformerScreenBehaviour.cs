193ff26521d2b9560c638ff2d2980f7542843c1d
Clement.R <rondeau.clement@gmail.com>
2024-11-17 14:32:06 +0100

    + implement informer screen flow

--- Assets/InformerScreenBehaviour.cs
+++ Assets/InformerScreenBehaviour.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 using UnityEngine.Networking;
 using TMPro;
 using System.Text.RegularExpressions;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class InformerScreenBehaviour : MonoBehaviour
 {
@@ -14,10 +16,55 @@ public class InformerScreenBehaviour : MonoBehaviour
 
     private string m_targetName;
 
+    [SerializeField]
+    private Button m_nextTargetButton;
+
+    [Header("Target Confirm Popup")]
+    [SerializeField]
+    private CanvasGroup m_popupCanvasGroup;
+    [SerializeField]
+    private Button m_returnButton;
+    [SerializeField]
+    private Button m_confirmButton;
+    [SerializeField]
+    private Animator m_fileAnimator;
+
     void Start()
     {
         m_targetPotato.enabled = false;
-        StartCoroutine(GenerateTargetName());
+        NextTarget();
+
+        SetPopupVisible(false);
+
+        m_returnButton.onClick.AddListener(() => { SetPopupVisible(false); });
+
+        m_nextTargetButton.onClick.AddListener(() => { SetPopupVisible(true); });
+
+        m_confirmButton.onClick.AddListener(NextTarget);
+    }
+
+    private void SetPopupVisible(bool p_visible)
+    {
+        m_popupCanvasGroup.alpha = p_visible ? 1f : 0f;
+        m_popupCanvasGroup.blocksRaycasts = p_visible;
+        m_popupCanvasGroup.interactable = p_visible;
+    }
+
+    private void NextTarget()
+    {
+        StartCoroutine(WaitForNextTarget());
+    }
+
+    private IEnumerator WaitForNextTarget()
+    {
+        m_nextTargetButton.gameObject.SetActive(false);
+        SetPopupVisible(false);
+        m_fileAnimator.SetBool("Visible", false);
+        yield return new WaitForSeconds(2.5f);
+        yield return GenerateTargetName();
+        m_fileAnimator.SetBool("Visible", true);
+        yield return new WaitForSeconds(2.5f);
+        m_nextTargetButton.gameObject.SetActive(true);
     }
 
     private IEnumerator GenerateTargetName()
